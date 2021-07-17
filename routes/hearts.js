4359d75f9ad5a5c234ae6f4ebb59a03f06dc4b4d
ayushskmaurya <ayushskmaurya@gmail.com>
2021-07-17 20:35:01 +0530

    Shuffled and distributed cards

--- routes/hearts.js
+++ routes/hearts.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
 	res.render('index', {css: 'index', js: 'index'});
+});
+
+router.get('/game', (req, res) => {
+	
+	deck = []
+	fs.readdirSync(path.join(__dirname, '../public/assets/cards')).forEach(card => {
+		deck.push(card);
+	});
+
+	// Shuffling the deck of cards.
+	for(let i=0; i<deck.length; i++) {
+		let j = Math.floor(Math.random() * i);
+		let temp = deck[i];
+		deck[i] = deck[j];
+		deck[j] = temp;
+	}
+	
+	res.render('game', {
+		player_cards: deck.splice(0, 13),
+		comp1_cards: deck.splice(0, 13),
+		comp2_cards: deck.splice(0, 13),
+		comp3_cards: deck.splice(0, 13),
+		css: 'game', js: false
+	});
 });
 
 module.exports = router;
